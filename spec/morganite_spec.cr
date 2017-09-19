f590f04a8ba34357148e63c83d88e4122fe2e584
Will Lewis <will@wflewis.com>
2017-09-19 01:30:03 -0400

    Add basic morganite functionality

--- spec/morganite_spec.cr
+++ spec/morganite_spec.cr
@@ -1,9 +1,41 @@
 require "./spec_helper"
 
 describe Morganite do
-  # TODO: Write tests
+  it "builds simple tags" do
+    m = Morganite::Morganite.new
 
-  it "works" do
-    false.should eq(true)
+    tag = m.div
+    tag.should eq("<div></div>")
+  end
+
+  it "builds tags with attributes" do
+    m = Morganite::Morganite.new
+
+    tag = m.div class: "foo"
+    tag.should eq("<div class=\"foo\"></div>")
+  end
+
+  it "builds tags with a subtag" do
+    m = Morganite::Morganite.new
+
+    tag = m.div { m.div }
+    tag.should eq("<div><div></div></div>")
+  end
+
+  it "builds tags with content" do
+    m = Morganite::Morganite.new
+
+    tag = m.div { "hello" }
+    tag.should eq("<div>hello</div>")
+  end
+
+  it "builds tags with multiple subtags" do
+    m = Morganite::Morganite.new
+
+    tag = m.div {
+      [ m.div { "hello" } ,
+        m.div { "world" } ].join
+    }
+    tag.should eq("<div><div>hello</div><div>world</div></div>")
   end
 end
--- src/morganite.cr
+++ src/morganite.cr
@@ -1,5 +1,37 @@
 require "./morganite/*"
 
 module Morganite
-  # TODO Put your code here
+
+  macro included
+    Morganite::Morganite.new
+  end
+
+  class Morganite
+    macro method_missing(call)
+      def {{call.name.id}}(**args)
+        tag({{call.name.id.stringify}}, **args) {% if call.block %} {{call.block}} {% else %} {} {% end %}
+      end
+    end
+
+    def tag(name, **args, &block)
+      o = [] of String
+
+      open_tag = [] of String
+      open_tag << "#{name}"
+
+      args.each do |i, j|
+        open_tag << "#{i}=\"#{j}\""
+      end
+
+      o << "<"
+      o << open_tag.join(" ").strip
+      o << ">"
+
+      b = yield
+      o << b if b
+
+      o << "</#{name}>"
+      o.join
+    end
+  end
 end
